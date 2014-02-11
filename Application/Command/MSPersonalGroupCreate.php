26660fd59c17973cde83a22935c430b7ee964ef4
Stiller13 <dimz13@rambler.ru>
2014-02-11 19:47:59 +0900

    база

---
+++ Application/Command/MSPersonalGroupCreate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Application\Command;
+
+class MSPersonalGroupCreate extends \System\Core\Command {
+
+	protected function exec() {
+
+		$session = new \System\Session\Session();
+		$user = $session->get('user');
+
+		$mg_type = 'personal';
+
+		$new_mg = new \Application\Model\PersonalMessageGroup();
+		$new_mg->setStatus(1);
+
+		$factory_group = \System\Orm\PersistenceFactory::getFactory('PersonalMessageGroup');
+		$group_finder = new \System\Orm\DomainObjectAssembler($factory_group);
+		$group_finder->insert($new_mg);
+
+//Вставка в группу пользователей
+		$ruleobj = new \Application\Model\RuleObj();
+
+		$ruleobj->setUser_id($user->getId());
+		$ruleobj->setObj_id($new_mg->getId());
+		$ruleobj->setRule(1);
+
+		$factory_ruleobj = \System\Orm\PersistenceFactory::getFactory('RuleObj');
+		$ruleobj_finder = new \System\Orm\DomainObjectAssembler($factory_ruleobj);
+		$ruleobj_finder->insert($ruleobj);
+
+//Вставка в visit-ов
+		$visit = new \Application\Model\Visit();
+		$visit->setMessageGroupId($new_mg->getId());
+		$visit->setUserId($user->getId());
+
+		$factory_visit = \System\Orm\PersistenceFactory::getFactory('Visit');
+		$visit_finder = new \System\Orm\DomainObjectAssembler($factory_visit);
+		$visit_finder->insert($visit);
+
+		return $this->redirect("/cabinet/message/personal/".$new_mg->getId());
+	}
+}
