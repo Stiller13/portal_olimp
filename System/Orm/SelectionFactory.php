50cd2bcba44edf5902bdd31a14979bcc701579b8
Stiller13 <dimz13@rambler.ru>
2014-02-14 14:35:37 +0900

    Удалил лишние команды Добавил путь к директории с шаблонами Smarty

--- System/Orm/SelectionFactory.php
+++ System/Orm/SelectionFactory.php
@@ -3,12 +3,13 @@ namespace System\Orm;
 
 class SelectionFactory{
 	function newSelection(IdentityObject $obj,$table){
-	    $core=$this->buildWhat($obj,$table).' ';
-        $core.=$obj->checkJoin();
-        list($where, $values)=$this->buildWhere($obj);
-        return array($core." ".$where.$obj->checkOrder().$obj->checkLimit(), $values);
+		$core=$this->buildWhat($obj,$table).' ';
+		$core.=$obj->checkJoin();
+		list($where, $values)=$this->buildWhere($obj);
+
+		return array($core." ".$where.$obj->checkOrder().$obj->checkLimit(), $values);
 	}
-	
+
 	function buildWhere(IdentityObject $obj){
 		if ($obj->isVoid()){
 			return array("",array());
@@ -19,19 +20,20 @@ class SelectionFactory{
 			$compstrings[]="{$comp['name']}{$comp['operator']}?";
 			$values[]=$comp['value'];
 		}
-        if ($obj->returnLink()){
-            $where=" WHERE ".implode(" AND ",$compstrings).' ';    
-        } else {$where=" WHERE ".implode(" OR ",$compstrings).' ';}
-		
+		if ($obj->returnLink()){
+			$where=" WHERE ".implode(" AND ",$compstrings).' ';    
+		} else {$where=" WHERE ".implode(" OR ",$compstrings).' ';}
+
 		return array($where,$values);
 	}
-    
-    function buildWhat(IdentityObject $obj, $table){
-        $fields= $obj->checkWhat();
-        $core=$core="SELECT $fields FROM $table";
-        // xlog($core);
-        return $core;
-    }
+
+	function buildWhat(IdentityObject $obj, $table){
+		$fields= $obj->checkWhat();
+		$core=$core="SELECT $fields FROM $table";
+		// xlog($core);
+
+		return $core;
+	}
 }
 
 ?>
