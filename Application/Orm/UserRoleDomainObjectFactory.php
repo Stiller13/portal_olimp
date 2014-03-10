3d5d80bd06b9669b9499cc840c823cb30254be09
Stiller13 <dimz13@rambler.ru>
2014-03-10 23:27:17 +0900

    fix typo

--- Application/Orm/UserRoleDomainObjectFactory.php
+++ Application/Orm/UserRoleDomainObjectFactory.php
@@ -2,15 +2,20 @@
 
 namespace Application\Orm;
 
-class UserRoleDomainObjectFactory extends \System\Orm\DomainObjectFactory{
-    function doCreateObject(array $array){
-        $obj= new \Application\Model\UserRole();
-	$obj->setUser_id($array['user_id']); 
-	$obj->setRole($array['role']);
-        return $obj;
-    }
-    
-    function targetClass(){
-        return "UserRole";
-    }
+class UserRoleDomainObjectFactory extends \System\Orm\DomainObjectFactory {
+
+	function doCreateObject(array $array) {
+		$obj = new \Application\Model\UserRole();
+
+		$obj->setId($array['role_id']);
+		$obj->setUser_id($array['role_user']); 
+		$obj->setRole(\System\Helper\Helper::getName("role", $array['role_role']));
+
+		return $obj;
+	}
+
+	function targetClass(){
+		return "UserRole";
+	}
+
 }
--- Application/Orm/UserRoleIdentityObject.php
+++ Application/Orm/UserRoleIdentityObject.php
@@ -1,10 +1,14 @@
 <?php
 namespace Application\Orm;
 
-class UserRoleIdentityObject extends \System\Orm\IdentityObject{
-    function __construct($field=null){
-        parent::__construct($field, array('user_id','role')); 
-    }
+class UserRoleIdentityObject extends \System\Orm\IdentityObject {
+
+	function __construct($field=null) {
+		parent::__construct($field, array(
+			'role_id',
+			'role_user',
+			'role_role')); 
+	}
 }
 
 
